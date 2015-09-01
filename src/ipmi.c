84290fbe43888f779ae478a66dd8e0643a78a484
Henrique Silva <henrique.silva@lnls.br>
2015-09-01 09:32:08 -0300

    src/ipmi.c: update ipmb function calls

--- src/ipmi.c
+++ src/ipmi.c
@@ -32,6 +32,7 @@
 #include "ipmb.h"
 #include "ipmi.h"
 #include "board_defs.h"
+#include "led.h"
 
 /* Local variables */
 QueueHandle_t ipmi_rxqueue = NULL;
@@ -73,6 +74,7 @@ void IPMITask ( void * pvParameters )
         default:
             break;
         }
+        prvToggleLED(LED_GREEN);
     }
 }
 
@@ -102,54 +104,57 @@ void ipmi_parse_bridge ( ipmi_msg * req )
 
 void ipmi_parse_se ( ipmi_msg * req )
 {
-    static uint8_t resp_buffer[IPMI_MAX_DATA_LEN];
-    /* Initialize to 0xFF so we know if an unsupported command could'nt be parsed */
-    uint8_t cc = 0xFF;
+    ipmi_msg resp;
+    /* Initialize to 0xFF so we know if an unsupported command couldn't be parsed */
+    resp.completion_code = 0xFF;
     uint8_t len = 0;
 
     switch (req->cmd) {
     case IPMI_SET_EVENT_RECEIVER_CMD:
         /* No data to return */
         /* TODO: We should send the sensors readings as events now */
-        cc = IPMI_CC_OK;
+        resp.data_len = 0;
+        resp.completion_code = IPMI_CC_OK;
         break;
 
     default:
         break;
     }
-    if ( cc != 0xFF ) {
-        ipmb_send_response(req, cc, resp_buffer, len);
+    if ( resp.completion_code != 0xFF ) {
+        ipmb_send_response(req, &resp);
     }
 }
 
 void ipmi_parse_app ( ipmi_msg * req )
 {
-    static uint8_t resp_buffer[IPMI_MAX_DATA_LEN];
-    uint8_t cc = 0xFF;
+    ipmi_msg resp;
+    /* Initialize to 0xFF so we know if an unsupported command couldn't be parsed */
+    resp.completion_code = 0xFF;
     uint8_t len = 0;
 
     switch (req->cmd) {
     case IPMI_GET_DEVICE_ID_CMD:
         /* Call a different function ? */
-        resp_buffer[len++] = 0x0A; /* Dev ID */
-        resp_buffer[len++] = 0x02; /* Dev Rev */
-        resp_buffer[len++] = 0x05; /* Dev FW Rev UPPER */
-        resp_buffer[len++] = 0x50; /* Dev FW Rev LOWER */
-        resp_buffer[len++] = 0x02; /* IPMI Version 2.0 */
-        resp_buffer[len++] = 0x1F; /* Dev Support */
-        resp_buffer[len++] = 0x5A; /* Manufacturer ID LSB */
-        resp_buffer[len++] = 0x31; /* Manufacturer ID MSB */
-        resp_buffer[len++] = 0x00; /* ID MSB */
-        resp_buffer[len++] = 0x01; /* Product ID LSB */
-        resp_buffer[len++] = 0x01; /* Product ID MSB */
-        cc = IPMI_CC_OK;
+        resp.data[len++] = 0x0A; /* Dev ID */
+        resp.data[len++] = 0x02; /* Dev Rev */
+        resp.data[len++] = 0x05; /* Dev FW Rev UPPER */
+        resp.data[len++] = 0x50; /* Dev FW Rev LOWER */
+        resp.data[len++] = 0x02; /* IPMI Version 2.0 */
+        resp.data[len++] = 0x1F; /* Dev Support */
+        resp.data[len++] = 0x5A; /* Manufacturer ID LSB */
+        resp.data[len++] = 0x31; /* Manufacturer ID MSB */
+        resp.data[len++] = 0x00; /* ID MSB */
+        resp.data[len++] = 0x01; /* Product ID LSB */
+        resp.data[len++] = 0x01; /* Product ID MSB */
+        resp.data_len = len;
+        resp.completion_code = IPMI_CC_OK;
         break;
 
     default:
         break;
     }
-    if ( cc != 0xFF ) {
-        ipmb_send_response(req, cc, resp_buffer, len);
+    if ( resp.completion_code != 0xFF ) {
+        ipmb_send_response(req, &resp);
     }
 }
 
@@ -170,29 +175,32 @@ void ipmi_parse_transport ( ipmi_msg * req )
 
 void ipmi_parse_picmg ( ipmi_msg * req )
 {
-    static uint8_t resp_buffer[IPMI_MAX_DATA_LEN];
-    uint8_t cc = 0xFF;
+    ipmi_msg resp;
+    /* Initialize to 0xFF so we know if an unsupported command couldn't be parsed */
+    resp.completion_code = 0xFF;
     uint8_t len = 0;
 
     switch (req->cmd) {
     case IPMI_PICMG_CMD_GET_PROPERTIES:
-        resp_buffer[len++] = IPMI_PICMG_GRP_EXT;
-        resp_buffer[len++] = 0x23;
-        resp_buffer[len++] = 0x01;
-        resp_buffer[len++] = 0x00;
-        cc = IPMI_CC_OK;
+        resp.data[len++] = IPMI_PICMG_GRP_EXT;
+        resp.data[len++] = 0x23;
+        resp.data[len++] = 0x01;
+        resp.data[len++] = 0x00;
+        resp.data_len = len;
+        resp.completion_code = IPMI_CC_OK;
         break;
 
     case IPMI_PICMG_CMD_SET_FRU_LED_STATE:
-        resp_buffer[len++] = IPMI_PICMG_GRP_EXT;
-        cc = IPMI_CC_OK;
+        resp.data[len++] = IPMI_PICMG_GRP_EXT;
+        resp.data_len = len;
+        resp.completion_code = IPMI_CC_OK;
         break;
 
     default:
         break;
     }
-    if ( cc != 0xFF ) {
-        ipmb_send_response(req, cc, resp_buffer, len);
+    if ( resp.completion_code != 0xFF ) {
+        ipmb_send_response(req, &resp);
     }
 }
 
