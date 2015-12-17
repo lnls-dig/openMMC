1c37e2e2dd694fcec4f20778f7d52c0e6e46b33c
Henrique Silva <henrique.silva@lnls.br>
2015-12-17 15:28:59 -0200

    Reformat the Cmake toolchain file

--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -1,18 +1,18 @@
-INCLUDE(CMakeForceCompiler)
+include(CMakeForceCompiler)
 
-SET(CMAKE_SYSTEM_NAME Generic)
-SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
+cmake_force_c_compiler(arm-none-eabi-gcc GNU)
+cmake_force_cxx_compiler(arm-none-eabi-g++ GNU)
 
-SET(CMAKE_FIND_ROOT_PATH "/usr/local/arm-none-eabi")
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH "/usr/local/arm-none-eabi")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-SET(COMMON_FLAGS "-fno-builtin -ffunction-sections -fdata-sections -fno-strict-aliasing -fmessage-length=0")
-SET(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
-SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=gnu++0x")
-SET(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections --specs=nosys.specs -nostdlib ")
+set(COMMON_FLAGS "-fno-builtin -ffunction-sections -fdata-sections -fno-strict-aliasing -fmessage-length=0")
+set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=gnu++0x")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections --specs=nosys.specs -nostdlib ")
