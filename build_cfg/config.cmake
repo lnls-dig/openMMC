3858fd9aaa0eee72f776af3daef7126dffa2fa3c
Henrique Silva <henrique.silva@lnls.br>
2016-01-11 16:22:05 -0200

    Add CMake rule to compile the flash_spi module when HPM is present

--- build_cfg/config.cmake
+++ build_cfg/config.cmake
@@ -22,6 +22,7 @@ if(${TARGET_BOARD} MATCHES "(afcv3_1)|(afcv3_0)") # (?i) makes the regexp case i
     "HOTSWAP_SENSOR"
     "TEMPERATURE_SENSOR"
     "VOLTAGE_SENSOR"
+    "HPM"
     #"RTM"
     )
 elseif(${TARGET_BOARD} MATCHES "(test)") # (?i) makes the regexp case insensitive
