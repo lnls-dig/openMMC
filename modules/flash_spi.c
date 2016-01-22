98bb01abf469acb0def5870174d00291e7fda85d
Henrique Silva <henrique.silva@lnls.br>
2016-01-22 09:13:35 -0200

    Implement additional functions for the external SPI Flash

--- modules/flash_spi.c
+++ modules/flash_spi.c
@@ -6,16 +6,18 @@
 
 void flash_write_enable( void )
 {
-    uint8_t tx_buff[1] = {FLASH_WRITE_ENABLE};
-
-    ssp_write( FLASH_SPI, tx_buff, sizeof(tx_buff) );
+    do {
+        uint8_t tx_buff[1] = {FLASH_WRITE_ENABLE};
+        ssp_write( FLASH_SPI, tx_buff, sizeof(tx_buff) );
+    } while (!(flash_read_status_reg() & 0x2));
 }
 
 void flash_write_disable( void )
 {
-    uint8_t tx_buff[1] = {FLASH_WRITE_DISABLE};
-
-    ssp_write( FLASH_SPI, tx_buff, sizeof(tx_buff) );
+    do {
+        uint8_t tx_buff[1] = {FLASH_WRITE_DISABLE};
+        ssp_write( FLASH_SPI, tx_buff, sizeof(tx_buff) );
+    } while (flash_read_status_reg() & 0x2);
 }
 
 void flash_read_id( uint8_t * id_buffer, uint8_t buff_size )
@@ -55,6 +57,39 @@ void flash_write_status_reg( uint8_t data )
     ssp_write( FLASH_SPI, &tx_buff[0], sizeof(tx_buff) );
 }
 
+uint8_t flash_read_lock_reg( uint32_t address )
+{
+    uint8_t tx_buff[4];
+
+    tx_buff[0] = FLASH_READ_LOCK_REG;
+    tx_buff[1] = (address >> 16) & 0xFF;
+    tx_buff[2] = (address >> 8) & 0xFF;
+    tx_buff[3] = address & 0xFF;
+
+    /* Size of rx buffer must be the sum of the length of the data expected to receive and the length of the sent data */
+    uint8_t rx_buff[5] = {0};
+
+    ssp_write_read( FLASH_SPI, &tx_buff[0], sizeof(tx_buff), &rx_buff[0], sizeof(rx_buff), portMAX_DELAY );
+
+    return rx_buff[4];
+}
+
+void flash_write_lock_reg( uint32_t address, uint8_t data )
+{
+    uint8_t tx_buff[5];
+
+    tx_buff[0] = FLASH_WRITE_LOCK_REG;
+    tx_buff[1] = (address >> 16) & 0xFF;
+    tx_buff[2] = (address >> 8) & 0xFF;
+    tx_buff[3] = address & 0xFF;
+    tx_buff[4] = data;
+
+    flash_write_enable();
+
+    ssp_write( FLASH_SPI, &tx_buff[0], sizeof(tx_buff) );
+}
+
+
 uint8_t flash_read_data( uint32_t address )
 {
     uint8_t tx_buff[4];
@@ -70,6 +105,27 @@ uint8_t flash_read_data( uint32_t address )
     ssp_write_read( FLASH_SPI, &tx_buff[0], 4, &rx_buff[0], 1, portMAX_DELAY );
 
     return rx_buff[4];
+}
+
+void flash_fast_read_data( uint32_t start_addr, uint8_t * dst, uint32_t size )
+{
+    /* Size of rx buffer must be the sum of the length of the data expected to receive and the length of the sent data */
+    /* Use an additional Dumb byte */
+    uint8_t *rx_buff = pvPortMalloc(size+5);
+
+    uint8_t tx_buff[5];
+
+    tx_buff[0] = FLASH_FAST_READ_DATA;
+    tx_buff[1] = (start_addr >> 16) & 0xFF;
+    tx_buff[2] = (start_addr >> 8) & 0xFF;
+    tx_buff[3] = start_addr & 0xFF;
+    tx_buff[4] = 0xFF; /* Dumb Byte */
+
+    ssp_write_read( FLASH_SPI, &tx_buff[0], 5, &rx_buff[0], size, portMAX_DELAY );
+
+    memcpy(dst, &rx_buff[5], size);
+
+    vPortFree(rx_buff);
 }
 
 void flash_program_page( uint32_t address, uint8_t * data, uint16_t size )
--- modules/flash_spi.h
+++ modules/flash_spi.h
@@ -1,7 +1,7 @@
 #ifndef FLASH_SPI_H_
 #define FLASH_SPI_H_
 
-#define FLASH_SPI_BITRATE                10000000
+#define FLASH_SPI_BITRATE                1000000
 #define FLASH_SPI_FRAME_SIZE             8
 
 /* M25P128 Flash commands */
@@ -10,6 +10,8 @@
 #define FLASH_READ_ID 0x9F
 #define FLASH_READ_STATUS_REG 0x05
 #define FLASH_WRITE_STATUS_REG 0x01
+#define FLASH_READ_LOCK_REG 0xE8
+#define FLASH_WRITE_LOCK_REG 0xE5
 #define FLASH_READ_DATA 0x03
 #define FLASH_FAST_READ_DATA 0x0B
 #define FLASH_PROGRAM_PAGE 0x02
@@ -22,9 +24,12 @@ void flash_read_id( uint8_t * id_buffer, uint8_t buff_size );
 uint8_t flash_read_status_reg( void );
 void flash_write_status_reg( uint8_t data );
 uint8_t flash_read_data( uint32_t address );
+void flash_fast_read_data( uint32_t start_addr, uint8_t * dst, uint32_t size );
 void flash_program_page( uint32_t address, uint8_t * data, uint16_t size );
 void flash_sector_erase( uint32_t address );
 void flash_bulk_erase( void );
+uint8_t flash_read_lock_reg( uint32_t address );
+void flash_write_lock_reg( uint32_t address, uint8_t data );
 uint8_t is_flash_busy( void );
 
 #endif
