bc42b9adea58af42a3dc2f859dd9c33f7c5b561b
Henrique Silva <henrique.silva@lnls.br>
2016-05-12 16:57:54 -0300

    Update FPGA diag initialization function label

--- modules/fpga_spi.c
+++ modules/fpga_spi.c
@@ -194,7 +194,7 @@ void vTaskFPGA_COMM( void * Parameters )
     }
 }
 
-void init_fpga_spi( void )
+void fpga_spi_init( void )
 {
     xTaskCreate(vTaskFPGA_COMM, "FPGA_COMM", 150, NULL, tskFPGA_COMM_PRIORITY, (TaskHandle_t *) NULL);
 }
