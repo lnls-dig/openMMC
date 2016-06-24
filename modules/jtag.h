28d038bd59ceb3ba5fdd213cbd177b9653e5853f
Henrique Silva <henrique.silva@lnls.br>
2016-06-24 13:20:24 -0300

    Move SCANSTA pin definitions to the boards' pin_mapping header

--- modules/jtag.h
+++ modules/jtag.h
@@ -22,16 +22,7 @@
 #ifndef JTAG_H_
 #define JTAG_H_
 
-#define SCANSTA_PORT            2
-
-#define SCANSTA_ADDR0_PIN       0
-#define SCANSTA_ADDR1_PIN       1
-#define SCANSTA_ADDR2_PIN       2
-#define SCANSTA_ADDR3_PIN       3
-#define SCANSTA_ADDR4_PIN       4
-#define SCANSTA_ADDR5_PIN       5
-#define SCANSTA_ADDR6_PIN       6
-#define SCANSTA_RST_PIN         7
+#include "pin_mapping.h"
 
 /**
  * @brief Initialize and configure SCANSTA addr pins
