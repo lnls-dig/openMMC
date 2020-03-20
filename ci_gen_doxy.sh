4a6eb90b28e5a72234832530381bb7e941e5ed63
Lucas Russo <lerwys@gmail.com>
2020-03-20 12:51:26 -0300

    ci_gen_dexy.sh: remove "build" directories for Doxygen

--- ci_gen_doxy.sh
+++ ci_gen_doxy.sh
@@ -6,6 +6,16 @@ set -e
 set -o
 
 if [ "${BUILD_ARTIFACT}" = "docs" ]; then
+    # remove build directories, as we can't
+    # use a file pattern in Doxyfile.dox file
+    # like */build*/* as travis seems to be
+    # expanding "*". This should be safe, as
+    # we only run this when BUILD_ARTIFACT is set
+    # to "docs" and not with "all_binaries", which
+    # is the release BUILD_ARTIFACT
+    rm -r $TRAVIS_BUILD_DIR/${BUILD_DIR}*
+
+    # Now generate the documentation
     cd $TRAVIS_BUILD_DIR/docs
     doxygen Doxyfile.dox
     echo "" > html/.nojekyll
