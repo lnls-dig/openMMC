dc3bf9d334c85e04f87d87d480968be76651013a
Lucas Russo <lerwys@gmail.com>
2021-03-16 16:35:51 -0300

    afc-fofb-ctrl: use ADN config compatible with afc-bpm
    In this way we can use this MMC firmware with both
    BPM projects and FOFB, as we don't change the BPM
    ADN used clocks, but just add another use for use
    by the P2P transceivers by FOFB.

--- port/board/afc-fofb-ctrl/v3_1/adn4604_usercfg.h
+++ port/board/afc-fofb-ctrl/v3_1/adn4604_usercfg.h
@@ -31,8 +31,8 @@
 #define ADN4604_CFG_OUT_3       0       /* TCLKB_OUT */
 #define ADN4604_CFG_OUT_4       13      /* FPGA_CCLK */
 #define ADN4604_CFG_OUT_5       8       /* FP2_CLK2 */
-#define ADN4604_CFG_OUT_6       15      /* LINK01_CLK */
-#define ADN4604_CFG_OUT_7       5       /* FP2_CLK1 */
+#define ADN4604_CFG_OUT_6       5       /* LINK01_CLK */
+#define ADN4604_CFG_OUT_7       15      /* FP2_CLK1 */
 #define ADN4604_CFG_OUT_8       8       /* PCIE_CLK1 */
 #define ADN4604_CFG_OUT_9       5       /* LINK23_CLK */
 #define ADN4604_CFG_OUT_10      5       /* FIN1_CLK3 */
