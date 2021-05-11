e95e8d40947b9a3998c1cb4954b8de422d6b8531
Augusto Fraga Giachero <afg@augustofg.net>
2021-05-11 14:16:50 -0300

    Declare i2c mapping tables as extern to avoid linking problems [afc-v4]

--- port/board/afc-v4/i2c_mapping.h
+++ port/board/afc-v4/i2c_mapping.h
@@ -50,8 +50,8 @@ enum {
 
 #define I2C_MUX_CNT    2
 
-i2c_mux_state_t i2c_mux[I2C_MUX_CNT];
-i2c_bus_mapping_t i2c_bus_map[I2C_BUS_CNT];
-i2c_chip_mapping_t i2c_chip_map[I2C_CHIP_CNT];
+extern i2c_mux_state_t i2c_mux[I2C_MUX_CNT];
+extern i2c_bus_mapping_t i2c_bus_map[I2C_BUS_CNT];
+extern i2c_chip_mapping_t i2c_chip_map[I2C_CHIP_CNT];
 
 #endif
