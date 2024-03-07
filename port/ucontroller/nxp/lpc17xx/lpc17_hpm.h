3e0d9e7792508fa8c7e649a76399fc0eb20914a2
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-03-07 10:04:05 -0300

    Add support to bootloader self-update

--- port/ucontroller/nxp/lpc17xx/lpc17_hpm.h
+++ port/ucontroller/nxp/lpc17xx/lpc17_hpm.h
@@ -34,9 +34,9 @@
 #define IPMC_UPDATE_SECTOR_END   0x11
 #define IPMC_UPDATE_ADDRESS_OFFSET (IPMC_UPDATE_SECTOR_START << 12)
 
-enum memory_area {
-    BOOT_FLASH,
-    FW_UPDATE_FLASH
+enum fw_type {
+    APPLICATION = 1,
+    BOOTLOADER = 2
 };
 
 uint8_t ipmc_hpm_prepare_comp(void);
@@ -44,7 +44,7 @@ uint8_t ipmc_hpm_upload_block(uint8_t *block, uint16_t size);
 uint8_t ipmc_hpm_finish_upload(uint32_t image_size);
 uint8_t ipmc_hpm_activate_firmware(void);
 uint8_t ipmc_hpm_get_upgrade_status(void);
-uint8_t program_page(uint32_t address, uint32_t *data, uint32_t size, enum memory_area area);
+uint8_t program_page(uint32_t address, uint32_t *data, uint32_t size);
 uint8_t ipmc_erase_sector(uint32_t sector_start, uint32_t sector_end);
 
 
